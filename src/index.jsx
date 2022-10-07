a0c060f2dd02fcc430c583997814300de6c44417
Eduardo Motta de Moraes <edumoraes@live.com>
2022-10-07 14:22:19 -0300

    change router basename

--- src/index.jsx
+++ src/index.jsx
@@ -7,7 +7,7 @@ import App from './components/App';
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <BrowserRouter basename="">
+    <BrowserRouter basename="/top-project-shopping-cart">
       <App />
     </BrowserRouter>
   </React.StrictMode>,
